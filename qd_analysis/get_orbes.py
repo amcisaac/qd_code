d3b69ddaf63753b5c653fa721177ee938b690f05
Lexie McIsaac <armcisaac@gmail.com>
2020-08-20 17:04:15 -0400

    get_orbes: added ability to get energies from opt cycle

--- qd_analysis/get_orbes.py
+++ qd_analysis/get_orbes.py
@@ -1,16 +1,25 @@
-import sys
+import argparse
 
 # script to extract orbital energies
 # usage: python3 get_orbes [name of TDDFT output file] > [CSV file]
 # (prints to terminal by default)
 
+my_parser = argparse.ArgumentParser(description='Get orbital energies from a QChem output file')
+my_parser.add_argument('inputfile',metavar='qchem file',help='The QChem output file')
+my_parser.add_argument('-c','--opt_cycle',action='store',help='Specify an optimization cycle to get orbitals from')
+my_parser.add_argument('-t','--triplet',action='store_true',help='Specify if the system is a triplet') # not implemented
 
+args=my_parser.parse_args()
 
-inputfile=sys.argv[1]  # output of TDDFT calculation
+# inputfile=sys.argv[1]  # output of TDDFT calculation
 
-with open(inputfile,'r') as inp:
+with open(args.inputfile,'r') as inp:
     flag=0  # flag indicates whether you're in the excitation energy part of the file
     flag2=0
+    if args.opt_cycle:
+        flag3 = 0
+    else:
+        flag3=1
     occ = "Occupied"
     for i,line in enumerate(inp):
         if flag==1:
@@ -18,7 +27,7 @@ with open(inputfile,'r') as inp:
             if flag2 == 1:
                 if line.find(' --------------------------------------------------------------') != -1:
                     break
-        
+
                 if line.find('Virtual') != -1:
                     occ = "Virtual"
                 else:
@@ -28,7 +37,11 @@ with open(inputfile,'r') as inp:
             if line.find('Occupied') != -1:
                 occ = "Occupied"
                 flag2=1
-        if line.find('Alpha MOs') != -1:
-            # print file header when you get to the orbital E's part, change flag to 1
-            print('Orbital energy (au),Occupation,'+inputfile)
-            flag = 1 
+        if flag3 ==1:
+            if line.find('Alpha MOs') != -1:
+                # print file header when you get to the orbital E's part, change flag to 1
+                print('Orbital energy (au),Occupation,'+args.inputfile)
+                flag = 1
+        if line.find('Optimization Cycle') != -1:
+            if line.split()[-1] == args.opt_cycle:
+                flag3 = 1
