1a3a8ea7f6d3df3443a8afdbf9c7f790f99f3e67
Lexie McIsaac <armcisaac@gmail.com>
2020-09-14 17:43:48 -0400

    documented pdos

--- qd_analysis/pdos.py
+++ qd_analysis/pdos.py
@@ -3,41 +3,69 @@ import sys
 from qd_helper import read_input_xyz,write_xyz
 from matplotlib import pyplot as plt
 
-def dos_grid(E_grid, sigma, E_orb, cik):
+def dos_grid(E_grid, sigma, E_orb, c):
+    '''
+    Function to calculate the weighted DOS based on the orbital energies
+    and fraction of the orbital on the core/shell.
+
+    Inputs:
+        E_grid: energy grid to calculate the DOS over
+        sigma: broadening parameter for the gaussians
+        E_orb: array of all the orbital energies
+        c: array of the fraction of each orbital on the core (or shell) atoms
+
+    Returns:
+        dos_grid: the gaussian braodened DOS, weighted by c
+    '''
     dos_grid=np.zeros(E_grid.shape)
-    for i in range(0,len(cik)):
-        dos_grid += (cik[i]/np.sqrt(2*np.pi*sigma**2))*np.exp(-(E_grid-E_orb[i])**2/(2*sigma**2))
+    for i in range(0,len(c)):
+        dos_grid += (c[i]/np.sqrt(2*np.pi*sigma**2))*np.exp(-(E_grid-E_orb[i])**2/(2*sigma**2))
     return dos_grid
 
 def make_mo_coeff_mat(mo_file_raw,nbas):
+    '''
+    Function to build the MO coefficient matrix from a QChem file
+
+    Inputs:
+        mo_file_raw: MO coefficient file from QChem (53.0), with each line having a new coefficient.
+        nbas: number of basis functions
+
+    Returns:
+        mo_coeff_mat: MO coefficient matrix with MO's as columns and AO's as rows
+                      (same order as atoms in xyz). Nbas x Nbas
+        mo_Es: array of MO energies
+    '''
     mo_coeff_mat = np.zeros((nbas,nbas))
     mo_Es = np.zeros(nbas)
     with open(mo_file_raw,'r') as mo_file:
         mo_lines=mo_file.readlines()
         for i in range(0,nbas): # row
             e_ind = 2 * nbas * nbas + i
-            mo_Es[i] = float(mo_lines[e_ind])
+            mo_Es[i] = float(mo_lines[e_ind]) # energy
             for j in range(0,nbas): # column
                 mo_ind = i * nbas + j
-                mo_coeff_mat[j,i] = float(mo_lines[mo_ind])
+                mo_coeff_mat[j,i] = float(mo_lines[mo_ind]) # coeff
     return mo_coeff_mat,mo_Es
 
-coeff_file = sys.argv[1]
-nbas=int(sys.argv[2])
-xyz_file=sys.argv[3]
-core_xyz_file = sys.argv[4]
 
-mo_mat,mo_e = make_mo_coeff_mat(coeff_file,nbas)
-print(mo_mat)
-print(mo_e)
+coeff_file = sys.argv[1] # hex version of qchem 53.0
+nbas=int(sys.argv[2])    # number of basis functions
+xyz_file=sys.argv[3]        # whole dot xyz file (must correspond to core coordinates)
+core_xyz_file = sys.argv[4] # core xyz file
 
 # number of orbitals per atom for each atom: 8 Se, 8 S, 18 Cd
 orb_per_atom_lanl2dz = {'Cd': 18, 'Se': 8, 'S': 8,'A':2, 'B': 3,'C':4}
 
+# get MO matrix and energies
+mo_mat,mo_e = make_mo_coeff_mat(coeff_file,nbas)
+# print(mo_mat)
+# print(mo_e)
+
+# read xyz files
 xyz,atoms=read_input_xyz(xyz_file)
 core_xyz,core_atoms=read_input_xyz(core_xyz_file)
 
-# UGH SO JANKY
+# get core/shell indices, and AO indices
 ind_core=np.full(atoms.shape,False)
 ind_core_ao = np.full(nbas,False)
 j=0
@@ -54,42 +82,32 @@ for i,coord in enumerate(xyz):
             ind_core[i]=True
             ind_core_ao[j:j+n_ao]=True
 
-
-
-
 ind_shell = np.logical_not(ind_core)
 ind_shell_ao = np.logical_not(ind_core_ao)
 
-# print(ind_core)
-# print(ind_core_ao)
-# print(ind_shell_ao)
-
-# print(mo_mat[ind_core_ao])
+# separate the MO matrix into core and shell contributions
 mo_mat_coreonly = mo_mat[ind_core_ao] # N MO (col) x N core AO's (row)
 mo_mat_shellonly = mo_mat[ind_shell_ao] # N MO (col) x N shell AO's (row)
-# print(mo_mat_coreonly[:,0])
+
+# sum down the columns to get the total fraction of the orbital on core/shell
 alpha_core = np.sum(mo_mat_coreonly,axis=0)
 alpha_shell = np.sum(mo_mat_shellonly,axis=0)
-# print(alpha_core)
+# check it adds to 1
 print(alpha_core+alpha_shell)
 
+# energy grid to evaluate the DOS over
 E_grid = np.arange(0.9*mo_e[0],1.1*mo_e[-1],0.01)
-# print(E_grid)
+# broadening parameter
 sigma=0.01
 
+# calculate projected DOS
 core_dos = dos_grid(E_grid,sigma,mo_e,alpha_core)
 shell_dos=dos_grid(E_grid,sigma,mo_e,alpha_shell)
 
+# plot PDOS
 plt.figure()
 plt.plot(E_grid,core_dos)
 plt.plot(E_grid,shell_dos)
 plt.stem(mo_e,alpha_core)
 plt.stem(mo_e, alpha_shell)
 plt.show()
-
-# ind_Cd = (atom_name_start == 'Cd')
-# ind_Se = (atom_name_start == 'Se')
-# ind_S  = (atom_name_start == 'S')
-# ind_chal = np.logical_or(ind_S,ind_Se)
-# ind_shell_Cd = np.logical_and(ind_shell,ind_Cd)
-# ind_core_Cd = np.logical_and(ind_core,ind_Cd)
